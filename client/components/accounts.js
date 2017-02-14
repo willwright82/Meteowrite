7adb269145962b4b44324535f91fe9a7c9cd1ae9
Will Wright <will@clazzoo.com>
2017-02-14 10:08:50 +0000

    Added Accounts component method for React to Handle Blaze's accounts interface

---
+++ client/components/accounts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Accounts extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default Accounts;
---
+++ client/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Header from './header';
+
+export default () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};

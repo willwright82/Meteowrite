145d234da8465138479b1e208fda703ed2aa6215
Will Wright <will@clazzoo.com>
2017-02-14 10:29:22 +0000

    Added Accounts component method for React to Handle Blaze's accounts interface

--- client/components/accounts.js
+++ client/components/accounts.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Template } from 'meteor/templating';
+import { Blaze } from 'meteor/blaze';
 
 class Accounts extends Component {
   componentDidMount() {
     // Render the Blaze accounts form
     // find div rendered in the `render` method
     // and place the Blaze accounts form in that div
+    this.view = Blaze.render( // this.view is a reference to rendered Blaze template
+      Template.loginButtons, // Login Forms from Blaze rendered
+      ReactDOM.findDOMNode(this.refs.container) // to ref="container"
+    );
   }
 
   componentWillUnmount() {
     // Find the form we created
     // Destroy the forms
     // Clean up the forms
+    Blaze.remove(this.view);
   }
 
   render() {
     return (
-      <div></div>
+      <div ref="container"></div>
     );
   }
 }
--- client/components/header.js
+++ client/components/header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Accounts from './accounts';
 
 class Header extends Component {
   render() {
@@ -8,7 +9,7 @@ class Header extends Component {
           <a className="navbar-brand" href="">Meteowrite</a>
         </div>
         <ul className="nav navbar-nav">
-          <li><a href="">Sign up</a></li>
+          <li><Accounts /></li>
           <li><a href="">Create document</a></li>
         </ul>
       </nav>

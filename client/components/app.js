850b83bd2e51ec4570556b3ed41049600195c6ed
Will Wright <will@clazzoo.com>
2017-02-17 12:17:04 +0000

    Added a root path route

--- client/components/app.js
+++ client/components/app.js
@@ -3,14 +3,14 @@ import React from 'react';
 import Header from './header';
 import DocsList from './docs/docs_list';
 
-export default () => {
+export default (props) => {
   return (
     <div>
       <Header />
       <div className="container-fluid">
         <div className="row">
           <div className="col-xs-12">
-            <DocsList />
+            {props.children}
           </div>
         </div>
       </div>
--- client/main.js
+++ client/main.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
-import Docs from '../imports/collections/docs';
+import DocsMain from './components/docs/docs_main';
+import DocsList from './components/docs/docs_list';
+import { Docs } from '../imports/collections/docs';
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="docs" component={DocsMain} />
+    </Route>
+  </Router>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.querySelector('.render-target'));
+  ReactDOM.render(routes, document.querySelector('.render-target'));
 });

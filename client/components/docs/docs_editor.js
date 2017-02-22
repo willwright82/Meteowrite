ee16ee5cada8b22dead9412e8ba62d20c3f2f875
Will Wright <will@clazzoo.com>
2017-02-22 15:57:21 +0000

    Added CodeMirror package for markdown editor

--- client/components/docs/docs_editor.js
+++ client/components/docs/docs_editor.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import CodeMirror from 'react-codemirror';
+import 'codemirror/mode/markdown/markdown';
 
 class DocsEditor extends Component {
   render() {
     return (
-      <div>Docs Editor</div>
+      <div className="col-sm-8">
+        <h5>Input</h5>
+        <CodeMirror options={{ mode: 'markdown', lineNumbers: true }} />
+      </div>
     );
   }
 }

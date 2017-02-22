65c728296dd54c2f4bd28a1389d1c189fa1ed871
Will Wright <will@clazzoo.com>
2017-02-22 15:44:06 +0000

    Added a Docs Editor Component

---
+++ client/components/docs/docs_editor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class DocsEditor extends Component {
+  render() {
+    return (
+      <div>Docs Editor</div>
+    );
+  }
+}
+
+export default DocsEditor;
--- client/components/docs/docs_main.js
+++ client/components/docs/docs_main.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Docs } from '../../../imports/collections/docs';
+import DocsEditor from './docs_editor';
 
 class DocsMain extends Component {
   render() {
+    //console.log(this.props.params.docId);
     return (
-      <div>DocsMain</div>
+      <div>
+        <DocsEditor doc={this.props.doc} />
+      </div>
     );
   }
 }
 
-export default DocsMain;
+export default createContainer((props) => {
+  const { docId } = props.params;
+  Meteor.subscribe('docs');
+
+  return { doc: Docs.findOne(docId) };
+}, DocsMain);

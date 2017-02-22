c44f08d18d3a6a5b366a5171a1dfe4a030da2acd
Will Wright <will@clazzoo.com>
2017-02-22 16:22:15 +0000

    Added markdown package to convert markdown to HTML

--- client/components/docs/docs_viewer.js
+++ client/components/docs/docs_viewer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { markdown } from 'markdown';
 
-class DocsViwer extends Component {
+class DocsViewer extends Component {
   render() {
+    const rawHTML = markdown.toHTML(this.props.doc.content);
+
     return (
       <div className="col-sm-4">
         <h5>Output</h5>
+        {rawHTML}
       </div>
     );
   }

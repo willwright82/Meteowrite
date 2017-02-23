c9560d64153a59e43b93816840cdfa0ca1f1aed9
Will Wright <will@clazzoo.com>
2017-02-23 14:28:47 +0000

    Added onShareClick event handler

--- client/components/docs/docs_share.js
+++ client/components/docs/docs_share.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 class DocsShare extends Component {
+  onShareClick() {
+    const email = this.refs.email.value;
+  }
+
   render() {
     return (
       <footer className="docs-share">
         <div className="input-group">
-          <input className="form-control" type="text" />
+          <input ref="email" className="form-control" type="text" />
           <div className="input-group-btn">
-            <button className="btn btn-default">Share Document</button>
+            <button
+              onClick={this.onShareClick.bind(this)}
+              className="btn btn-default">
+              Share Document
+            </button>
           </div>
         </div>
       </footer>

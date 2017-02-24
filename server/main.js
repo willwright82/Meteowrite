9cbb757dea05ae71394124143327329f8b647ab1
Will Wright <will@clazzoo.com>
2017-02-24 12:13:37 +0000

    Publish 'sharedDocs' of an user

--- server/main.js
+++ server/main.js
@@ -6,4 +6,16 @@ Meteor.startup(() => {
   Meteor.publish('docs', function () {
     return Docs.find({ ownerId: this.userId });
   });
+
+  Meteor.publish('sharedDocs', function () {
+    const user = Meteor.users.findOne(this.userId);
+
+    if (!user) { return; }
+
+    const email = user.emails[0].address;
+
+    return Docs.find({
+      sharedWith: { $elemMatch: { $eq: email } }
+    });
+  });
 });
